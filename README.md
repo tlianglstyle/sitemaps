38e468f7ad7827261f7bee684eeba6ae36f5673a
谭亮 <tanliang@gaiay.cn>
2018-11-06 18:28:24 +0800

    favicon

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 ``` bash
 # install dependencies
 npm install
-```
 npm start
+```
 
-sitemaps:[http://47.105.62.221/sitemaps/index.html]
+sitemaps: [sitemaps](http://47.105.62.221/sitemaps/index.html)
--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { Button, WhiteSpace } from 'antd-mobile';
 import Item from './Item';
 import styles from './App.css';
 const data =[
-  {title:'亚马逊' , src: require('./assets/images/img_19.jpg') , href:'../amazon-logistics/index.html' },
-  {title:'众人添财' , src: require('./assets/images/img_21.jpg') , href:'../tc/index.html' },
+  {title:'亚马逊' , src: require('./assets/images/img_19.jpg') , href:'../../amazon-logistics/index.html' },
+  {title:'众人添财' , src: require('./assets/images/img_21.jpg') , href:'../../tc/index.html' },
   {title:'叮当加速度，元宵送祝福！' , src: require('./assets/images/img_17.jpg') , href:'http://mobile.lenovo-idea.com/2015/offline/lanternfestival/' },
-  {title:'让爱传递,她的达芬奇密码' , src: require('./assets/images/img_18.jpg') , href:'../women/index.html' },
+  {title:'让爱传递,她的达芬奇密码' , src: require('./assets/images/img_18.jpg') , href:'../../women/index.html' },
   {title:'厨房巅峰挑战赛' , src: require('./assets/images/img_20.jpg') , href:'http://mobile.lenovo-idea.com/2015/offline/cooking/' },
-  {title:'联想小新笔记本' , src: require('./assets/images/img_22.jpg') , href:'../telday/index.html' },
+  {title:'联想小新笔记本' , src: require('./assets/images/img_22.jpg') , href:'../../telday/index.html' },
   {title:'360全景图' , src: require('./assets/images/img_test1.jpg') , href:'http://mobile.lenovo-idea.com/2015/test/360outer/' },
   {title:'联想大客户 电信日' , src: require('./assets/images/img_23.jpg') , href:'http://mobile.lenovo-idea.com/2015/telecomday/' },
   {title:'联想高考祈福H5' , src: require('./assets/images/img_24.jpg') , href:'http://mobile.lenovo-idea.com/2015/bless/' },
@@ -39,12 +39,17 @@ const data =[
   {title:'联想智能电视携手哆啦A梦发红包赠玩偶送电视啦！(下线)' , src: require('./assets/images/img_16.jpg') , href:'' },
   {title:'联想大客户 端午节送红包(下线)' , src: require('./assets/images/img_25.jpg') , href:'' }
 ]
+
 let pageId = 1
+let pageSize = 4
+if(window.innerWidth>768){
+  pageSize = 12
+}
 const fecthData = ()=>{
   const _pageId = pageId
   pageId++
   return data.filter((value,index)=>{
-    if(index>(_pageId-1)*4 && index<=(_pageId)*4) return value
+    if(index>=(_pageId-1) * pageSize && index<=(_pageId) * pageSize ) return value
   })
 }
 class App extends Component {
